e919cf1e66c6ce607e7b2f1f55d145b35c6e0845
Deepak <deepaklakinana@gmail.com>
2026-05-29 23:06:49 +0530

    final project commit

---
+++ panda_programs/p9.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = {
+    'Name': ['nithin', 'nithya', 'nikhil', 'nishanth', 'nihal'],
+    'Age': [20, 22, 22, 20, 23],
+    'Subject': ['be', 'msc', 'bca', 'mtech', 'bsc'],
+    'Marks': [85.5, 80.5, 95.5, 75.5, 65.5]
+}
+
+df = pd.DataFrame(data)
+
+grouped = df.groupby('Age')['Salary'].mean() 
+
+
+print(grouped)
